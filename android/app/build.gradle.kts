04fa2acaa0ad0bfa6711f71f54bca45dcc25a7dd
DELL <harismentorperformance@gmail.com>
2026-04-23 18:08:50 +0530

    first_push_on_230426

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cristal_webapp"
+    namespace = "com.techzera.cristal_webapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cristal_webapp"
+        applicationId = "com.techzera.cristal_webapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/cristal_webapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cristal_webapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
